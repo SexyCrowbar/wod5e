cbce7b7431424449510f8a8bddc6923a21adb3b6
Veilza <113429647+Veilza@users.noreply.github.com>
2026-01-05 08:43:47 -0600

    Fix data model inheritance of SPC sheet
    Resolves #272

--- system/actor/data-models/fields/setting-fields.js
+++ system/actor/data-models/fields/setting-fields.js
@@ -13,7 +13,13 @@ export function settingFields() {
         tenets: new fields.BooleanField({ initial: false })
       }),
 
-      skillAttributeInputs: new fields.BooleanField({ initial: false })
+      skillAttributeInputs: new fields.BooleanField({ initial: false }),
+
+      // SPC-specific settings
+      enableGifts: new fields.BooleanField({ initial: false }),
+      enableDisciplines: new fields.BooleanField({ initial: false }),
+      enableEdges: new fields.BooleanField({ initial: false }),
+      generalDifficultyEnabled: new fields.BooleanField({ initial: true })
     })
   }
 }
--- system/actor/data-models/spc-actor-model.js
+++ system/actor/data-models/spc-actor-model.js
@@ -1,15 +1,12 @@
 import { WoDActorModel } from './base-actor-model.js'
 import { standardDiceFields } from './fields/attribute-fields.js'
-import { hunterFields } from './fields/hunter-fields.js'
 import { skillFields } from './fields/skill-fields.js'
-import { vampireFields } from './fields/vampire-fields.js'
-import { werewolfFields } from './fields/werewolf-fields.js'
 
 export class SPCActorModel extends WoDActorModel {
   static defineSchema() {
     const fields = foundry.data.fields
 
-    const schema = {}
+    const schema = super.defineSchema()
 
     // The type of group
     schema.spcType = new fields.StringField({ initial: 'mortal' })
@@ -28,29 +25,11 @@ export class SPCActorModel extends WoDActorModel {
       strongest: new fields.NumberField({ initial: 0 })
     })
 
-    // Header fields
-    schema.headers = new fields.SchemaField({
-      concept: new fields.StringField({ initial: '' })
-    })
-
     // Standard Dice fields
     schema.standarddicepools = new fields.SchemaField(standardDiceFields())
 
     // Exceptional Dice fields
     schema.exceptionaldicepools = new fields.SchemaField(skillFields())
-
-    // Settings
-    schema.settings = new fields.SchemaField({
-      enableGifts: new fields.BooleanField({ initial: false }),
-      enableDisciplines: new fields.BooleanField({ initial: false }),
-      enableEdges: new fields.BooleanField({ initial: false }),
-      generalDifficultyEnabled: new fields.BooleanField({ initial: true })
-    })
-
-    // Splat-specific fields
-    Object.assign(schema, vampireFields())
-    Object.assign(schema, werewolfFields())
-    Object.assign(schema, hunterFields())
 
     return schema
   }
